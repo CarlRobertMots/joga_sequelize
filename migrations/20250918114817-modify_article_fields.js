2acbab167d2823ea0162e4c0232358d3be51111e
CarlRobertMots <robertm6ts@gmail.com>
2025-09-18 15:11:08 +0300

    Created article table and added table data

---
+++ migrations/20250918114817-modify_article_fields.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    queryInterface.changeColumn('Articles', 'id', {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    });
+    queryInterface.changeColumn('Articles', 'name', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    queryInterface.changeColumn('Articles', 'slug', {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      unique: true
+    });
+    queryInterface.changeColumn('Articles', 'body', {
+      type: Sequelize.TEXT,
+      allowNull: false
+    });
+    queryInterface.changeColumn('Articles', 'image', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    queryInterface.changeColumn('Articles', 'published', {
+      type: Sequelize.DATE,
+      allowNull: false
+    });
+    queryInterface.changeColumn('Articles', 'author_id', {
+      type: Sequelize.INTEGER,
+    
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.dropTable('Articles');
+  }
+};

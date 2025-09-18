40420127c62746ec0af6dcebddd2f2a8edd24d57
CarlRobertMots <robertm6ts@gmail.com>
2025-09-18 13:48:01 +0300

    Create connection to database

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+const { configDotenv } = require("dotenv")
 const express = require("express")
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true }))
+
+//connect to db
+configDotenv()
+    const { Sequelize } = require('sequelize');
+    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DRIVER
+});
+//test db connection
+    sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Database connected...")
+    })
+    .catch(err => {
+        console.log("Error: " + err)
+    })
 
 app.get("/", (req,res) => {
     res.json({ message: "Welcome to sequelize app"} )
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000")
+app.listen(3027, () => {
+    console.log("Server is running on http://localhost:3027")
 } )
